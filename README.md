0eba41dd67bbd831795394a0bfe0e745a3fcb351
魏庆涛 <weiqingtao@dwnews.com>
2020-08-25 11:41:05 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # netease-off-schedule
 网易云音乐定时关闭
+由于下班走的匆忙，所以常常忘记关掉网易云音乐，导致歌曲放一晚上，不仅浪费电而且第二天一来电脑烫的要死，所以写了该定时程序，每天下午六点半定时关掉网易云
+
+## 运行 
+node programOff.js
+
+pm2 守护进程的方式：
+pm2 start programOff.js
+设置开机启动：
+pm2 startup
+pm2 save
---
+++ programOff.js
@@ -0,0 +1,14 @@
+const shell = require('shelljs');
+const fs = require('fs');
+const schedule = require('node-schedule');
+console.log("program is starting!!程序会在每天下午六点定时关闭![Yeah!]");
+const time = '0 00 18 * * ?' //每天的两点35分执行一次
+schedule.scheduleJob(time, function () {
+    const version = shell.exec('ps aux | grep NeteaseMusic', { silent: true }).stdout; //获取程序运行信息
+    fs.writeFileSync('message.txt', version); //把运行信息写入文件
+    const message = shell.exec('head -1 message.txt | tail -1', { silent: true }).stdout; //获取文件第一行
+    const port = message.split(" ")[7];
+    shell.exec(`kill ${port}`, { silent: true }).stdout; //获取程序运行信息
+    fs.unlinkSync("./message.txt")
+    console.log('Netease Music is killed');
+});
